af768ac3174ea6d4522f2b87c9192348cea6965c
unknown <500075823@stu.upes.ac.in>
2021-05-09 14:15:00 +0530

    Established Serial Communication

--- CovInspect_NodeMCU/CovInspect_NodeMCU.ino
+++ CovInspect_NodeMCU/CovInspect_NodeMCU.ino
@@ -1,14 +1,18 @@
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
+#include <SoftwareSerial.h>
 
+SoftwareSerial s(D6, D5);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 void setup() {
   Serial.begin(9600);
+  s.begin(9600);
   mlx.begin();
 }
 
 void loop() {
   Serial.println(mlx.readObjectTempF() + 7);
+  Serial.println(s.read());
   delay(1000);
 }
