0084db9d8e7474f73fe9191fed20534c609ce4bb
unknown <500075823@stu.upes.ac.in>
2021-05-09 15:14:58 +0530

    Integrated with API

--- CovInspect_NodeMCU/CovInspect_NodeMCU.ino
+++ CovInspect_NodeMCU/CovInspect_NodeMCU.ino
@@ -1,6 +1,11 @@
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = "Trikuldham";
+const char* password = "jaihanuman1981";
 
 SoftwareSerial s(D6, D5);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
@@ -9,10 +14,25 @@ void setup() {
   Serial.begin(9600);
   s.begin(9600);
   mlx.begin();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting..");
+  }
 }
 
 void loop() {
   Serial.println(mlx.readObjectTempF() + 7);
-  Serial.println(s.read());
+  String hr = String(s.read());
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin("http://x.herokuapp.com/entrypoint?username=neeltron&hr="+hr+"&ir="+String(mlx.readObjectTempF()));
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+    http.end();
+  }
   delay(1000);
 }
